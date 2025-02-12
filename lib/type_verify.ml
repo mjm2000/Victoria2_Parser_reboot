a6e02c4ba773b94f7b865480c9f7979284e54d0a
mjm2000 <holtzmanjimmy@yahoo.com>
2025-02-12 16:00:22 -0600

    Commit on run

--- lib/type_verify.ml
+++ lib/type_verify.ml
@@ -160,7 +160,7 @@ let rec type_verify_r symbol_table assignments exceptions scope =
                 (v :: exceptions)
             )
          | (x) -> 
-            Printf.printf "Error here:%s" (string_assignment_list (ASSIGNMENT_LIST(ls)));
+            Printf.printf "Error here:%s" (Pre_parser.string_assignment_list (ASSIGNMENT_LIST(ls)));
             let e = UNEXPECTED_ASSIGN_LIST(ls) in
             ((RHS[x],e,cords)::exceptions)
         in
