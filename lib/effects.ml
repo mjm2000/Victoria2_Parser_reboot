65affa8dea9cd7d54af682b579f01b4900cb4ba5
mjm2000 <holtzmanjimmy@yahoo.com>
2025-02-12 17:09:55 -0600

    Commit on run

--- lib/effects.ml
+++ lib/effects.ml
@@ -203,6 +203,14 @@ PARAM_VALUE(SCOPE)
 (TYPE_SYMBOL(INT),PROVINCE_EFFECTS);
 ]
 let province_effects = symbol_table_init [ 
+(KEYWORD_SYMBOL("add_country_modifier"),PARAM_LIST(symbol_table_init [
+    (KEYWORD_SYMBOL("name"),PARAM_OPTION([
+        PARAM_VALUE(KEYWORD);
+        PARAM_VALUE(STRING)
+    ]);
+    );
+    (KEYWORD_SYMBOL("duration"),PARAM_VALUE(INT));
+]));
 
 (KEYWORD_SYMBOL("random_country"),COUNTRY_EFFECTS);
 (KEYWORD_SYMBOL("random_state"),STATE_EFFECTS);
