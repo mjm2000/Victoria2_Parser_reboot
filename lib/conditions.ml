d517e30074d643e7aae25c3a6cf3b1874b3d6d74
mjm2000 <holtzmanjimmy@yahoo.com>
2025-02-13 03:40:30 -0600

    Commit on run

--- lib/conditions.ml
+++ lib/conditions.ml
@@ -47,7 +47,7 @@ let pop_conditions = symbol_table_init [
 (TYPE_SYMBOL(CONDITION), POP_CONDITIONS);
 ]
 let country_conditions = symbol_table_init [
-((*maybe errors*)
+(*maybe errors*)
 (KEYWORD_SYMBOL("is_slave"),PARAM_VALUE(BOOL));
 (KEYWORD_SYMBOL("world_wars_enabled"),PARAM_VALUE(BOOL));
 (TYPE_SYMBOL(SCOPE),COUNTRY_CONDITIONS);
