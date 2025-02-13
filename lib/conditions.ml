ba1a0ab96031d06b06dc2795faedaa751da06486
mjm2000 <holtzmanjimmy@yahoo.com>
2025-02-13 03:06:35 -0600

    Commit on run

--- lib/conditions.ml
+++ lib/conditions.ml
@@ -435,6 +435,7 @@ let province_conditions = symbol_table_init [
 ]));
 (KEYWORD_SYMBOL("world_wars_enabled"),PARAM_VALUE(BOOL));
 (*maybe errors*)
+(KEYWORD_SYMBOL("has_province_modifier"),PARAM_VALUE(KEYWORD));
 (KEYWORD_SYMBOL("culture"),PARAM_VALUE(KEYWORD));
 (KEYWORD_SYMBOL("has_country_flag"),PARAM_VALUE(KEYWORD));
 (KEYWORD_SYMBOL("has_country_modifier"),PARAM_VALUE(KEYWORD));
