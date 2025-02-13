7f3019c12bd9b554c8340e90d4e82c3720765168
mjm2000 <holtzmanjimmy@yahoo.com>
2025-02-13 03:49:17 -0600

    Commit on run

--- lib/conditions.ml
+++ lib/conditions.ml
@@ -223,7 +223,10 @@ let country_conditions = symbol_table_init [
 (KEYWORD_SYMBOL("political_movement_strength"),PARAM_VALUE(INT));
 (KEYWORD_SYMBOL("political_reform_name"),PARAM_VALUE(KEYWORD));
 (KEYWORD_SYMBOL("political_reform_want"),PARAM_VALUE(FLOAT));
-(KEYWORD_SYMBOL("poor_strata_everyday_needs"),PARAM_VALUE(FLOAT));
+(KEYWORD_SYMBOL("poor_strata_everyday_needs"),PARAM_OPTION([
+    PARAM_VALUE(FLOAT);
+    PARAM_VALUE(INT);
+]));
 (KEYWORD_SYMBOL("poor_strata_life_needs"),PARAM_OPTION([
     PARAM_VALUE(FLOAT);
     PARAM_VALUE(INT);
