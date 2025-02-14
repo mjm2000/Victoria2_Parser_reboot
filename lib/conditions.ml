a9a323c180c310faecd9c18ddbc1a99d40592a9b
mjm2000 <holtzmanjimmy@yahoo.com>
2025-02-14 02:34:36 -0600

    Commit on run

--- lib/conditions.ml
+++ lib/conditions.ml
@@ -110,7 +110,10 @@ let country_conditions = symbol_table_init [
 (KEYWORD_SYMBOL("capital"),PARAM_VALUE(INT));
 (KEYWORD_SYMBOL("casus_belli"),PARAM_VALUE(KEYWORD));
 (KEYWORD_SYMBOL("citizenship_policy"),PARAM_VALUE(KEYWORD));
-(KEYWORD_SYMBOL("civilization_progress"),PARAM_VALUE(INT));
+(KEYWORD_SYMBOL("civilization_progress"),PARAM_OPTION([
+    PARAM_VALUE(FLOAT);
+    PARAM_VALUE(INT);
+]));
 (KEYWORD_SYMBOL("civilized"),PARAM_VALUE(BOOL));
 (KEYWORD_SYMBOL("colonial_nation"),PARAM_VALUE(BOOL));
 (KEYWORD_SYMBOL("constructing_cb_progress"),PARAM_VALUE(INT));
