816736cb59a1843aa0bfdc0ea3ac789a35db741d
mjm2000 <holtzmanjimmy@yahoo.com>
2025-02-15 17:07:12 -0600

    Commit on run

--- lib/type_verify.ml
+++ lib/type_verify.ml
@@ -161,15 +161,15 @@ let rec type_verify_r symbol_table assignments exceptions scope =
                     Printf.printf "(Error here:%s)\n" (Pre_parser.string_symbol_table symbol_table_i);
                     Printf.printf "(Error here:%s)\n" (Pre_parser.string_assignment_list ls);
                     print_endline "----------------------------------------";
+                    (if KEYWORD_SYMBOL("from") = "move_issue_percentage" then
+                        print_endline "Yes";
+                    else
+                        print_endline "No";
+                    );
+
                     );
 
                 type_verify_r symbol_table_i ls [] (RHS([new_scope]))
-
-                
-                
-                
-                
-                
                 
             ) symbol_tables    
             in
