c020c28747306ebc94c3873f84ce48e023477358
mjm2000 <holtzmanjimmy@yahoo.com>
2025-02-15 17:00:58 -0600

    Commit on run

--- lib/type_verify.ml
+++ lib/type_verify.ml
@@ -119,10 +119,9 @@ let rec type_verify_r symbol_table assignments exceptions scope =
         let exceptions = assign_type_check rh exceptions in
         type_verify_r symbol_table rest exceptions scope
     |None ->
-        if lh_value == "from" then
-            (print_endline "from";
-            print_endline (Pre_parser.string_symbol_table symbol_table);
-            );
+        print_endline lh_value; 
+        print_endline (Pre_parser.string_symbol_table symbol_table);
+            
         let e = UNKNOWN_IDENTIFIER(lh_value) in             
         type_verify_r symbol_table rest ((scope,e,cords)::exceptions) scope
     )
