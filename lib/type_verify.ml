1433719dcdaaaa4507fa37c30096200e4417c386
mjm2000 <holtzmanjimmy@yahoo.com>
2025-02-15 15:05:28 -0600

    Commit on run

--- lib/type_verify.ml
+++ lib/type_verify.ml
@@ -162,7 +162,7 @@ let rec type_verify_r symbol_table assignments exceptions scope =
                 then
                     Printf.printf "Error here:%s" (Pre_parser.string_assignment_list ls);
                     (*print_scope*)
-                    Printf.printf "Error here:%s" (string_rh_symbol scope);
+                    Printf.printf "Error here:%s" (Pre_parser.string_rh_symbol scope);
 
                 let v = (RHS([new_scope]),(MULTIPLE_CHOICE(exception_lists)),cords) in
                 (v :: exceptions)
