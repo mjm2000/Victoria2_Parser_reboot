6ab4aba49bfca3ec15e88a55e75456d2720edd13
mjm2000 <holtzmanjimmy@yahoo.com>
2025-02-15 17:11:06 -0600

    Commit on run

--- lib/type_verify.ml
+++ lib/type_verify.ml
@@ -155,12 +155,13 @@ let rec type_verify_r symbol_table assignments exceptions scope =
             let exception_lists = List.map (fun symbol_table_i -> 
                 (
                 if lh_value = "move_issue_percentage" then
-
+                    (*
                     print_endline "----------------------------------------";
                     Printf.printf "(Error here:%s)\n" (Pre_parser.string_symbol_table symbol_table_i);
                     Printf.printf "(Error here:%s)\n" (Pre_parser.string_assignment_list ls);
                     print_endline "----------------------------------------";
-                    if Hashtbl.mem symbol_table_i (KEYWORD_SYMBOL "from") then print_endline "Yes";
+                    *)
+                    if not (Hashtbl.mem symbol_table_i (KEYWORD_SYMBOL "from")) then print_endline "Yes";
 
                 );
 
