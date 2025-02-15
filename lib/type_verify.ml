cd65dffb1425408b0669ed431809eb1523f01066
mjm2000 <holtzmanjimmy@yahoo.com>
2025-02-15 17:20:36 -0600

    Commit on run

--- lib/type_verify.ml
+++ lib/type_verify.ml
@@ -153,16 +153,17 @@ let rec type_verify_r symbol_table assignments exceptions scope =
             let symbol_tables = symbol_table_from_rhv new_scope in
 
             let exception_lists = List.map (fun symbol_table_i -> 
+
+                    (*
                 (
                 if lh_value = "move_issue_percentage" then
-                    (*
                     print_endline "----------------------------------------";
                     Printf.printf "(Error here:%s)\n" (Pre_parser.string_symbol_table symbol_table_i);
                     Printf.printf "(Error here:%s)\n" (Pre_parser.string_assignment_list ls);
                     print_endline "----------------------------------------";
-                    *)
 
                 );
+                *)
 
                 type_verify_r symbol_table_i ls [] (RHS([new_scope]))
                 
