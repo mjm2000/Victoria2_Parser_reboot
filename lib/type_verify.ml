228f3839c50eae967f97d49294a5864d8a9ff18a
mjm2000 <holtzmanjimmy@yahoo.com>
2025-02-15 17:54:14 -0600

    Commit on run

--- lib/type_verify.ml
+++ lib/type_verify.ml
@@ -128,7 +128,7 @@ let rec type_verify_r symbol_table assignments exceptions scope =
             print_endline lh_value;
             if not (Hashtbl.mem symbol_table (KEYWORD_SYMBOL lh_value)) then print_endline "Not found" else print_endline "Found";
             (print_endline (Pre_parser.string_symbol_table symbol_table));
-    )
+    *)
         
         let e = (UNKNOWN_IDENTIFIER(lh_value) ) in             
         type_verify_r symbol_table rest ((scope,e,cords)::exceptions) scope
