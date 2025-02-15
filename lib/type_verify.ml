4fe38b927869359b15e6e247c27fc50d6c753e88
mjm2000 <holtzmanjimmy@yahoo.com>
2025-02-15 17:50:21 -0600

    Commit on run

--- lib/type_verify.ml
+++ lib/type_verify.ml
@@ -77,14 +77,11 @@ let rec type_verify_r symbol_table assignments exceptions scope =
      let expected_rh_type = 
          match lh_type with
          |KEYWORD when (Hashtbl.mem symbol_table (KEYWORD_SYMBOL(lh_value)) )  -> 
-            if not (Hashtbl.mem symbol_table (KEYWORD_SYMBOL lh_value)) then print_endline "Not found";
 
             Hashtbl.find_opt symbol_table (KEYWORD_SYMBOL(lh_value)) 
          |any_type when Hashtbl.mem symbol_table (TYPE_SYMBOL(any_type)) ->  
             Hashtbl.find_opt symbol_table (TYPE_SYMBOL(any_type))
          |_-> 
-
-            if not (Hashtbl.mem symbol_table (KEYWORD_SYMBOL lh_value)) then print_endline "Not found";
             None
      in
      let rec assign_type_check expected_rh_type exceptions =  
@@ -125,10 +122,9 @@ let rec type_verify_r symbol_table assignments exceptions scope =
         let exceptions = assign_type_check rh exceptions in
         type_verify_r symbol_table rest exceptions scope
     |None ->
-           (* print_endline lh_value;
+            print_endline lh_value;
             if not (Hashtbl.mem symbol_table (KEYWORD_SYMBOL lh_value)) then print_endline "Not found" else print_endline "Found";
             (print_endline (Pre_parser.string_symbol_table symbol_table));
-    *)
         
         let e = (UNKNOWN_IDENTIFIER(lh_value) ) in             
         type_verify_r symbol_table rest ((scope,e,cords)::exceptions) scope
