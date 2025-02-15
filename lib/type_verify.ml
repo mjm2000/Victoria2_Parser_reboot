47fbb2702bbf2bf4d9e66cc842a194b897690d83
mjm2000 <holtzmanjimmy@yahoo.com>
2025-02-15 16:25:37 -0600

    Commit on run

--- lib/type_verify.ml
+++ lib/type_verify.ml
@@ -175,6 +175,7 @@ let rec type_verify_r symbol_table assignments exceptions scope =
 
             type_verify_r symbol_table rest exceptions scope
         |None -> 
+            Printf.printf "Error here:%s" (lh_value);
             let e = UNKNOWN_IDENTIFIER(lh_value) in
             type_verify_r symbol_table rest ((scope,e,cords)::exceptions) scope
         )
