97329f08bd4ee4770d4428cc41d1937cbebf8cd8
mjm2000 <holtzmanjimmy@yahoo.com>
2025-02-16 17:22:49 -0600

    Commit on run

--- lib/conditions.ml
+++ lib/conditions.ml
@@ -185,7 +185,11 @@ let country_conditions = symbol_table_init [
 (KEYWORD_SYMBOL("has_recently_lost_war"),PARAM_VALUE(BOOL));
 (KEYWORD_SYMBOL("has_unclaimed_cores"),PARAM_VALUE(BOOL));
 (KEYWORD_SYMBOL("ideology"),PARAM_VALUE(KEYWORD));
-(KEYWORD_SYMBOL("industrial_score"),PARAM_VALUE(INT));
+(KEYWORD_SYMBOL("industrial_score"),PARAM_OPTION([
+    PARAM_VALUE(INT);
+    PARAM_VALUE(TAG);
+    PARAM_VALUE(SCOPE)
+]));
 (KEYWORD_SYMBOL("in_sphere"),PARAM_OPTION([
         PARAM_VALUE(TAG);
         PARAM_VALUE(SCOPE)
