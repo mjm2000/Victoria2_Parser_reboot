79a5acf02101f5bc947a78a87fe8a736a6f272d9
mjm2000 <holtzmanjimmy@yahoo.com>
2025-02-15 18:22:15 -0600

    Commit on run

--- lib/effects.ml
+++ lib/effects.ml
@@ -414,6 +414,7 @@ let province_effects = symbol_table_init [
 ]
 
 let state_effects = symbol_table_init [
+(KEYWORD_SYMBOL("remove_province_modifier"),PARAM_VALUE(KEYWORD));
 (KEYWORD_SYMBOL("any_pop"),POP_EFFECTS);
 (KEYWORD_SYMBOL("change_region_name"),PARAM_VALUE(STRING));
 (KEYWORD_SYMBOL("flashpoint_tension"),PARAM_VALUE(INT));
--- lib/type_verify.ml
+++ lib/type_verify.ml
@@ -155,16 +155,6 @@ let rec type_verify_r symbol_table assignments exceptions scope =
 
             let exception_lists = List.map (fun symbol_table_i -> 
 
-                    (*
-                (
-                if lh_value = "move_issue_percentage" then
-                    print_endline "----------------------------------------";
-                    Printf.printf "(Error here:%s)\n" (Pre_parser.string_symbol_table symbol_table_i);
-                    Printf.printf "(Error here:%s)\n" (Pre_parser.string_assignment_list ls);
-                    print_endline "----------------------------------------";
-
-                );
-                *)
 
                 type_verify_r symbol_table_i ls [] (RHS([new_scope]))
                 
@@ -191,7 +181,6 @@ let rec type_verify_r symbol_table assignments exceptions scope =
 
             type_verify_r symbol_table rest exceptions scope
         |None -> 
-            Printf.printf "Error: %s\n" lh_value;
             let e = UNKNOWN_IDENTIFIER(lh_value) in
 
             type_verify_r symbol_table rest ((scope,e,cords)::exceptions) scope
