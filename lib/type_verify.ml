4b15102f591dada0cd8a59704b5d247f05ce2205
mjm2000 <holtzmanjimmy@yahoo.com>
2025-02-18 02:22:37 -0600

    Commit on run

--- lib/type_verify.ml
+++ lib/type_verify.ml
@@ -159,9 +159,9 @@ let rec type_verify_r symbol_table assignments exceptions scope =
          | (PARAM_OPTION(_) as new_scope) ->
             let symbol_tables = symbol_table_from_rhv new_scope in
 
-            let exception_lists = fold_until_empty (fun symbol_table_i -> 
+            let exception_lists = fold_until (fun symbol_table_i -> 
                 type_verify_r symbol_table_i ls [] (RHS([new_scope]))
-            ) [] symbol_tables    (fun acc -> acc == []) 
+            ) [] symbol_tables 
             in
             (match exception_lists with
             |[] -> exceptions
