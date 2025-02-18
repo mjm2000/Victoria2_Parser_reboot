06eea0f2267767aa2c7ab528c4f8d5b7b8e26fde
mjm2000 <holtzmanjimmy@yahoo.com>
2025-02-18 02:23:00 -0600

    Commit on run

--- lib/type_verify.ml
+++ lib/type_verify.ml
@@ -21,7 +21,7 @@ let rec fold_until_empty f acc lst =
   match lst with
   | [] -> acc
   | [] :: xs -> []
-  | x::xs -> fold_until f (f acc x) xs
+  | x::xs -> fold_until_empty f (f acc x) xs
 
 let split_list n ls =
     let rec split_list_r lh rh i = match rh with
@@ -159,7 +159,7 @@ let rec type_verify_r symbol_table assignments exceptions scope =
          | (PARAM_OPTION(_) as new_scope) ->
             let symbol_tables = symbol_table_from_rhv new_scope in
 
-            let exception_lists = fold_until (fun symbol_table_i -> 
+            let exception_lists = fold_until_empty (fun symbol_table_i -> 
                 type_verify_r symbol_table_i ls [] (RHS([new_scope]))
             ) [] symbol_tables 
             in
