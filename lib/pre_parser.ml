6ea4f8880c26b6e6e4471d4a1b426356b93d2a33
mjm2000 <holtzmanjimmy@yahoo.com>
2025-02-19 03:16:44 -0600

    Commit on run

--- lib/pre_parser.ml
+++ lib/pre_parser.ml
@@ -1,7 +1,7 @@
 open Lexer
 open Type_def
 
-let rec assignments ls = 
+let assignments ls = 
     let rec assignments_r out rest = match rest with 
         | [] -> (List.rev out)
         | lexems -> 
