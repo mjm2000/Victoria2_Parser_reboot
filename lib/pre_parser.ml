5ddd409459d9ea645f077f4f98baa522559c7cca
mjm2000 <holtzmanjimmy@yahoo.com>
2025-02-19 03:08:00 -0600

    Commit on run

--- lib/pre_parser.ml
+++ lib/pre_parser.ml
@@ -82,7 +82,11 @@ and exception_iden_string exp = match exp with
 
     | UNEXPECTED_RIGHT_BRACKET -> "Unexpected Right Bracket"
     | UNEXPECTED_LEFT_BRACKET -> "Unexpected Left Bracket"
-    | MULTIPLE_CHOICE(_)-> "Multiple Choice "
+    | MULTIPLE_CHOICE(ls)-> 
+        let x = List.fold_left (fun acc x -> 
+            Printf.sprintf "%s\n\t%s" acc x
+        ) "" ls in
+        Printf.sprintf "Multiple Choices(%s)" x 
 
 and exception_string (exp : exception_value) : string = 
     (*add expected value printer*)
