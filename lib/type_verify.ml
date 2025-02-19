d7767002aeacc56ecb0c4c2d248f4aba59a237e9
mjm2000 <holtzmanjimmy@yahoo.com>
2025-02-19 02:32:20 -0600

    Commit on run

--- lib/type_verify.ml
+++ lib/type_verify.ml
@@ -93,7 +93,7 @@ let rec type_verify_r symbol_table assignments exceptions scope =
         |PARAM_OPTION(options) as rh ->
 
             let rec shortest_exception_list_r options expanded_exceptions = match options with 
-                |[] -> (CHOICE(expanded_exceptions))::exceptions
+                |[] -> (MULTIPLE_CHOICE(expanded_exceptions))::exceptions
                 |top::rest ->
                     (match assign_type_check top expanded_exceptions with
                     |[] -> exceptions 
