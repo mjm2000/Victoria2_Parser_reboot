8ebb715ea0a0b2406946ff2cdef91c726d9312e8
mjm2000 <holtzmanjimmy@yahoo.com>
2025-02-18 23:37:12 -0600

    restore

--- lib/lexer.ml
+++ lib/lexer.ml
@@ -10,19 +10,14 @@ type lexem_type =
 |TAG  
 |SCOPE
 |CONDITION
+|LEX_ERROR
 
 type lexem = lexem_type * string * (int * int)
 
 
-let read_file_str f : char list= 
-    let ic = open_in f in 
-    let rec read_all file out = 
-    try 
 
-        read_all  file  (input_char ic::out)
-    with  _ ->   out 
-    in 
-    List.rev (read_all ic []) 
+
+
 let ls_to_str ls = 
     let buf = Buffer.create (List.length ls)in
     List.iter (Buffer.add_char buf) ls; 
@@ -35,38 +30,55 @@ let str_to_ls str =
 let match_reg reg str= Str.string_match (Str.regexp reg)  str 0  
 let chars_to_lexem cl= match (cl) with 
           |iv when match_reg "-?[0-9]+$" iv -> INT
-          |fv when match_reg "-?[0-9]+\\.[0-9]+$" fv -> FLOAT 
-          |"NOT"|"AND"|"OR" -> CONDITION 
-          |tag when match_reg  "[A-Z][A-Z][A-Z]" tag-> TAG 
+          |fv when match_reg "-?[0-9]*\\.[0-9]+$" fv -> FLOAT 
+          |"NOT"|"AND"|"OR"|"not"|"and"|"or" -> CONDITION 
+          |tag when match_reg  "^[A-Z][A-Z][A-Z]$" tag->
+                  TAG 
           |"yes"|"no" -> BOOL 
-          |"FROM"|"THIS" -> SCOPE 
+          |"FROM"|"THIS"|"this"|"from" -> SCOPE 
           |_-> KEYWORD
 
-let lexer file = 
-    let rec lex_r (chrs:char list) (buf:char list) out (x,y) = 
-        let str = ls_to_str (List.rev buf) in
-        match (chrs,buf) with
-        |'\"'::rs,'\"'::_ ->  
-                let str = ls_to_str ('\"'::List.rev buf) in
-                lex_r rs [] ((STRING,str,(x,y)) :: out) (x+1,y)
-        |chr::rs,'\"'::rob -> lex_r rs ('\"'::chr::rob) out (x+1,y)
-        |'#'::rs,_->lex_r rs ['#'] out (x+1,y)
-        |'\n'::rs,['#'] -> lex_r rs [] out (1,y+1) 
-        |_::rs,['#'] ->   lex_r rs ['#'] out (x+1,y)
-        |'='::rs,_  -> lex_r rs [] ((EQ,str,(x,y)) ::out)  (x+1,y)
-        |'{'::rs, _-> lex_r rs [] ((LB,str,(x,y))::out) (x+1,y)
-        |'}'::rs, _-> lex_r rs [] ((RB,str,(x,y))::out)        (x+1,y)
-        |'\n'::rs,[] -> lex_r rs [] out(1,y+1)
-        |'\n'::rs,_ -> lex_r rs [] (((chars_to_lexem str),str,(x,y)) :: out) (1,y+1)
-        |' '::rs,[]|'\t'::rs,[] -> lex_r rs [] out (x+1,y)
-        |' '::rs,_|'\t'::rs,_-> 
-            lex_r rs [] ((chars_to_lexem str,str,(x,y)) :: out) (x+1,y)
-
-        |chr::rs,buf ->  lex_r rs (chr::buf)  out (x+1,y)
-        |[],[]->out 
-        |[],_-> ((chars_to_lexem str),str,(x,y)) :: out
-        in
-        List.rev (lex_r  (read_file_str file)  [] [] (1,1))
+let lexer f = 
+    let ic = open_in f in 
+    let rec read_all buf out (y,x) = 
+    match (buf) with
+        |'#'::rs ->
+            (match input_char ic with
+            |'\n' -> read_all [] out (y+1,1)
+            |_ -> read_all ('#'::rs) out (y,x+1)
+            |exception End_of_file -> close_in ic; out;
+            )
+        |'\"'::rs ->  
+            (match input_char ic with
+            |'\"' -> let str = ls_to_str ('\"'::List.rev buf) in                  
+                read_all [] ((STRING,str,(y,x)) :: out)  (y,x+1)
+            |chr-> 
+                read_all ('\"'::chr::rs) out  (y,x+1)
+            |exception End_of_file -> close_in ic; out;
+            )
+        |buf_chrs ->
+            let updated_out = (match buf_chrs with
+            |[]->out
+            |buf_chrs -> 
+                let string = ls_to_str (List.rev buf_chrs) in
+                let buffer_token = (chars_to_lexem string, string,(y,x-1)) in
+                buffer_token::out
+            )
+            in
+            (match input_char ic with
+            |'{' -> read_all [] ((LB,"{",(y,x))::updated_out) (y,x+1)
+            |'}' -> read_all [] ((RB,"}",(y,x))::updated_out) (y,x+1)
+            |'=' -> read_all [] ((EQ,"=",(y,x))::updated_out) (y,x+1)
+            |'#' -> read_all ['#'] (updated_out) (y,x+1)
+            |'\n' -> read_all [] (updated_out) (y+1,1)
+            |'\r' -> read_all [] (updated_out) (y,x)
+            |' '|'\t' -> read_all [] updated_out (y,x+1)
+            |chr -> read_all (chr::buf_chrs) out (y,x+1)
+            |exception End_of_file -> close_in ic; out;
+            )
+        |exception End_of_file -> close_in ic; out;
+    in 
+    List.rev (read_all [] [] (1,1)) 
 
 let lexem_to_str lexem =match lexem with 
 | FLOAT ->    "Float" 
@@ -80,6 +92,22 @@ let lexem_to_str lexem =match lexem with
 | SCOPE ->    "Scope"
 | CONDITION ->"Conditional"
 | BOOL ->     "Bool"      
+| LEX_ERROR ->"Lex Error"
 
-let print_lexem lexem = Printf.printf "%s:" (lexem_to_str lexem)
-let print_lexems lexems = List.iter (fun (lex,str,(x,y))->print_lexem lex; Printf.printf "%s (%i,%i)\n" str x y;) lexems;;
+
+let string_lexem lex_value = 
+  let (lex, str, (x, y)) = lex_value in
+  Printf.sprintf "%s %s (%i,%i)" (lexem_to_str lex) str x y
+
+
+let string_lexems lexems = 
+  List.fold_left (fun buffer lexem -> Printf.sprintf "%s\n%s" buffer (string_lexem lexem)) "" lexems
+
+let print_lexem lexem = 
+  Printf.printf "%s: " (lexem_to_str lexem)
+
+let print_lexems lexems = 
+  List.iter (fun (lex, str, (x, y)) ->
+    print_lexem lex;
+    Printf.printf "%s (%i,%i)\n" str x y
+  ) lexems
