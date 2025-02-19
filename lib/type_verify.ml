7441351366f1f386887d71c9192ab523189f1f1f
mjm2000 <holtzmanjimmy@yahoo.com>
2025-02-19 02:47:05 -0600

    Commit on run

--- lib/type_verify.ml
+++ lib/type_verify.ml
@@ -5,7 +5,6 @@ open Type_def
 open Mtth
 (*This function takes a list of exceptions and returns the shortest list of exceptions*)
 (*The rh is a list*)
-let my_var = ref 0
 
 let shortest_list lists =
   let rec find_shortest (shortest, shortest_len) = function
@@ -156,9 +155,6 @@ let rec type_verify_r symbol_table assignments exceptions scope =
             *)
             List.length symbol_tables |> string_of_int|> print_endline;
             let exception_lists = List.map (fun symbol_table ->
-                my_var := !my_var + 1; 
-                print_endline (lh_value);
-                print_endline (string_of_int !my_var);
                 type_verify_r symbol_table ls [] (RHS([new_scope]))
             ) symbol_tables 
 
