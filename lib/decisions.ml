bdf10ca56450da9696e1b910dbc81a497e667adc
mjm2000 <holtzmanjimmy@yahoo.com>
2025-02-22 16:50:34 -0600

    Commit on run

--- lib/decisions.ml
+++ lib/decisions.ml
@@ -6,6 +6,10 @@ let decisions = symbol_table_init [
             (KEYWORD_SYMBOL "potential",COUNTRY_CONDITIONS);
             (KEYWORD_SYMBOL "allow",COUNTRY_CONDITIONS);
             (KEYWORD_SYMBOL "effect",COUNTRY_EFFECTS);
+            (KEYWORD_SYMBOL "news",PARAM_VALUE(BOOL));
+            (KEYWORD_SYMBOL "news_desc_short",PARAM_VALUE(STRING));
+            (KEYWORD_SYMBOL "news_desc_medium",PARAM_VALUE(STRING));
+            (KEYWORD_SYMBOL "news_desc_large",PARAM_VALUE(STRING));
             (KEYWORD_SYMBOL("ai_will_do"),PARAM_LIST(symbol_table_init [
                 (KEYWORD_SYMBOL("factor"),
                     PARAM_OPTION([
