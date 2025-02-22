35535d6f3494358e5b065da513ba44d65e7d50ee
mjm2000 <holtzmanjimmy@yahoo.com>
2025-02-21 23:23:03 -0600

    Commit on run

--- bin/main.ml
+++ bin/main.ml
@@ -30,10 +30,10 @@ let () =
     let doc = "My_program does nothing but output 'cow'." in
     let info = Cmd.info "my_program" ~doc in
     let files_arg title doc =
-        Arg.(value & opt (list string) [] & info [title] ~doc)
+        Arg.(value & opt (some (list string) ) None & info [title] ~doc)
     in
 let filter_files raw_files selected_files = match selected_files with
-    |Some files -> List.filter (fun x -> Array.mem x files) raw_files
+    |Some files -> List.filter (fun x -> List.mem x files) raw_files
     |None -> raw_files
     in
     
