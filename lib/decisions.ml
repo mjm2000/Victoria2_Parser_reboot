23eed655bebd9cef88ee2b819a43ce748868c5f4
mjm2000 <holtzmanjimmy@yahoo.com>
2025-02-22 16:51:36 -0600

    Commit on run

--- lib/decisions.ml
+++ lib/decisions.ml
@@ -7,6 +7,7 @@ let decisions = symbol_table_init [
             (KEYWORD_SYMBOL "allow",COUNTRY_CONDITIONS);
             (KEYWORD_SYMBOL "effect",COUNTRY_EFFECTS);
             (KEYWORD_SYMBOL "news",PARAM_VALUE(BOOL));
+            (KEYWORD_SYMBOL "news_title",PARAM_VALUE(STRING));
             (KEYWORD_SYMBOL "news_desc_short",PARAM_VALUE(STRING));
             (KEYWORD_SYMBOL "news_desc_medium",PARAM_VALUE(STRING));
             (KEYWORD_SYMBOL "news_desc_large",PARAM_VALUE(STRING));
