3530c3b10c9abbc52abfdbcf09504a52f426eb98
mjm2000 <holtzmanjimmy@yahoo.com>
2025-02-22 00:49:32 -0600

    Commit on run

--- lib/type_verify.ml
+++ lib/type_verify.ml
@@ -121,7 +121,6 @@ let rec type_verify_r symbol_table assignments exceptions scope =
         type_verify_r symbol_table rest exceptions scope
     |None ->
 
-        print_endline (lh_value);
         let e = (UNKNOWN_IDENTIFIER(lh_value) ) in             
         type_verify_r symbol_table rest ((scope,e,cords)::exceptions) scope
     )
@@ -203,6 +202,7 @@ let rec type_verify_r symbol_table assignments exceptions scope =
 
     
     | [] -> 
+            Printf.printf "except:%s\n" (Pre_parser.exceptions_string exceptions); 
            exceptions
 in
 List.rev (type_verify_r symbol_table assignments [] (RHS([PARAM_LIST(symbol_table)])))
