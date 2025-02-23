440e694407593aa5357e6f71157b56912b7f1272
mjm2000 <holtzmanjimmy@yahoo.com>
2025-02-22 23:46:21 -0600

    Commit on run

--- lib/commons.ml
+++ lib/commons.ml
@@ -3,11 +3,11 @@ open Type_def
 
 let bookmarks = symbol_table_init [
     (KEYWORD_SYMBOL, "bookmark",PARAM_LIST(symbol_table_init [
-        (KEYWORD_SYMBOL, "name", PARAM_VALUE(STRING));
-        (KEYWORD_SYMBOL, "desc", PARAM_VALUE(STRING)); 
-        (KEYWORD_SYMBOL, "date", PARAM_VALUE(KEYWORD));
-        (KEYWORD_SYMBOL, "cameraX", PARAM_VALUE(INT));
-        (KEYWORD_SYMBOL, "cameraY", PARAM_VALUE(INT));
+        (KEYWORD_SYMBOL "name", PARAM_VALUE(STRING));
+        (KEYWORD_SYMBOL "desc", PARAM_VALUE(STRING)); 
+        (KEYWORD_SYMBOL "date", PARAM_VALUE(KEYWORD));
+        (KEYWORD_SYMBOL "cameraX", PARAM_VALUE(INT));
+        (KEYWORD_SYMBOL "cameraY", PARAM_VALUE(INT));
     ])
     );
 
