c050b9c961bf4b9820a591aa139c6f70f06f18ae
mjm2000 <holtzmanjimmy@yahoo.com>
2025-02-24 02:10:41 -0600

    Commit on run

--- lib/type_verify.ml
+++ lib/type_verify.ml
@@ -79,7 +79,7 @@ let rec type_verify_r symbol_table assignments exceptions scope =
         let rec assign_type_check expected_rh_type ls exceptions =  
             match expected_rh_type,ls with
             |_,[] -> exceptions
-            |LEXEM_LIST(erh_type),(LEXEM(rh_type,rh_value,cords)::rest) when erh_type = lh_type  -> 
+            |LEXEM_LIST(erh_type),(LEXEM(rh_type,rh_value,cords)::rest) when erh_type = rh_type  -> 
                 assign_type_check erh_type rest exceptions
             |_ -> 
                 let e = UNEXPECTED_EXPR_LIST(ls) in
