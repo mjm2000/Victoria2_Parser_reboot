e57d2ba1276327defd8363ff0659638b8a6f92ab
mjm2000 <holtzmanjimmy@yahoo.com>
2025-03-07 01:02:31 -0600

    Commit on run

---
+++ lib/SyntaxChecker/type_verify.ml
@@ -0,0 +1,265 @@
+open Victoria2
+open Symbol_table
+open Victoria2.Effects
+open Victoria2.Conditions
+open Victoria2.Modifiers
+open Type_def
+open Victoria2.Mtth
+(*This function takes a list of exceptions and returns the shortest list of exceptions*)
+(*The rh is a list*)
+
+let shortest_list lists =
+  let rec find_shortest (shortest, shortest_len) = function
+    | [] -> shortest
+    | hd :: tl ->
+        let current_length = List.length hd in
+        if current_length < shortest_len then
+          find_shortest (Some hd, current_length) tl
+        else
+          find_shortest (shortest, shortest_len) tl
+  in
+  find_shortest (None, max_int) lists
+
+
+let split_list n ls =
+    let rec split_list_r lh rh i = match rh with
+    |hd::rest when i < n -> 
+        split_list_r (hd::lh) rest (i+1)
+    |_->
+        ((List.rev lh),rh)
+    in
+
+    split_list_r [] ls 0
+
+let rec symbol_table_from_rhv rhv = match rhv with
+    |(PROVINCE_MTTH) ->
+        [province_mtth]
+    | (COUNTRY_MTTH) ->
+        [country_mtth]
+    | (PROVINCE_EFFECTS) ->
+        [province_effects]
+    | (PROVINCE_CONDITIONS) ->
+        [province_conditions]
+    | (COUNTRY_EFFECTS) ->
+        [country_effects]
+    | (COUNTRY_CONDITIONS) ->
+        [country_conditions]
+    | (POP_EFFECTS) ->
+        [pop_effects]
+    | (POP_CONDITIONS) ->
+        [pop_conditions]
+    | (STATE_EFFECTS) ->
+        [state_effects]
+    | (STATE_CONDITIONS) ->
+        [state_conditions]
+    | COUNTRY_MODIFIERS ->
+        [country_modifiers]
+    | PROVINCE_MODIFIERS ->
+        [province_modifiers]
+    | (PARAM_LIST(sub_table))->
+        [sub_table]
+    | PARAM_OPTION(options) ->
+        List.fold_left (fun acc rh ->
+            (symbol_table_from_rhv rh)@acc
+        ) [] options 
+
+    | APPEND_SYMBOLS(appended_symbols,param_value) -> 
+
+        let param_symbol_tables = symbol_table_from_rhv param_value  
+        in
+        List.map (fun symbol_table -> append_table symbol_table appended_symbols) param_symbol_tables
+    | _ -> []
+
+let type_verify symbol_table assignments =
+let rec type_verify_r symbol_table assignments exceptions scope = 
+    match assignments with
+    |ASSIGNMENT((lh_type,lh_value,assign_cords), LEXEM_LIST(ls))::rest ->
+        let expected_rh_type = 
+            match lh_type with
+            |KEYWORD when (Hashtbl.mem symbol_table (KEYWORD_SYMBOL(lh_value)) )  -> 
+               Hashtbl.find_opt symbol_table (KEYWORD_SYMBOL(lh_value)) 
+            |any_type when Hashtbl.mem symbol_table (TYPE_SYMBOL(any_type)) ->  
+               Hashtbl.find_opt symbol_table (TYPE_SYMBOL(any_type))
+            |_-> None
+        in
+        let rec assign_type_check (expected_rh_type:rh_symbol_type) ls exceptions =  
+            match expected_rh_type,ls with
+            |_,[] -> exceptions
+            |VALUE_LIST(list_erh_type),(LEXEM(rh_type,rh_value,cords)::rest)  -> 
+                (match list_erh_type with
+                |PARAM_VALUE(erh_type) when erh_type = rh_type  -> 
+                    assign_type_check expected_rh_type rest exceptions
+                |PARAM_VALUE(erh_type) when erh_type !=  rh_type  ->
+                    let e = (TYPE_MISHMASH(lh_value, erh_type, rh_type)) in
+                    let new_exceptions=((RHS [PARAM_VALUE(erh_type)],e,cords)::exceptions) in
+                    assign_type_check expected_rh_type rest new_exceptions
+                |PARAM_OPTION(options) ->
+                    let rec shortest_exception_list_r options = match options with 
+                        |[] -> ((RHS options,UNEXPECTED_LEXEM(rh_value,rh_type),cords)::exceptions)
+                        |top::rest_types ->
+                            (match (assign_type_check top rest []) with
+                            |[] -> exceptions 
+                            |_-> shortest_exception_list_r rest_types 
+                            )
+                    in
+                    let exceptions = shortest_exception_list_r options in
+                    assign_type_check expected_rh_type rest exceptions
+                |_ ->
+                    let e = UNEXPECTED_LEXEM(rh_value,rh_type) in
+                    let expected = (RHS [expected_rh_type]) in
+                    ((expected,e,cords)::exceptions)
+                )
+            |_,_ -> 
+                let e = UNEXPECTED_EXPR_LIST(ls) in
+                let expected = (RHS [expected_rh_type]) in
+                let new_exceptions =((expected,e,assign_cords)::exceptions) in
+                assign_type_check expected_rh_type ls new_exceptions
+
+        in
+        (match expected_rh_type with
+            |Some rhs ->  let new_exceptions = assign_type_check rhs ls exceptions in
+                type_verify_r symbol_table rest new_exceptions scope
+            |None -> 
+                let e = (UNKNOWN_IDENTIFIER(lh_value) ) in             
+                type_verify_r symbol_table rest ((scope,e,assign_cords)::exceptions) scope
+            )
+
+    |ASSIGNMENT((lh_type,lh_value,_), LEXEM((rh_type,rh_value,cords)))::rest  ->
+     let expected_rh_type = 
+         match lh_type with
+         |KEYWORD when (Hashtbl.mem symbol_table (KEYWORD_SYMBOL(lh_value)) )  -> 
+            Hashtbl.find_opt symbol_table (KEYWORD_SYMBOL(lh_value)) 
+         |_ when (Hashtbl.mem symbol_table (KEYWORD_SYMBOL(lh_value)))  -> 
+            Hashtbl.find_opt symbol_table (KEYWORD_SYMBOL(lh_value)) 
+         |any_type when Hashtbl.mem symbol_table (TYPE_SYMBOL(any_type)) ->  
+            Hashtbl.find_opt symbol_table (TYPE_SYMBOL(any_type))
+         |_-> 
+            None
+     in
+     let rec assign_type_check expected_rh_type exceptions =  
+        match expected_rh_type with
+        |PARAM_VALUE(erh_type) when erh_type = rh_type  -> 
+            (*
+            Printf.printf "%s:%d %d scope:%s\n" lh_value x y (Pre_parser.string_expected_value scope);
+            *)
+            exceptions 
+        |(PARAM_VALUE(erh_type) as epv) when erh_type !=  rh_type  ->
+           let e = (TYPE_MISHMASH(lh_value, erh_type, rh_type)) in
+           (((RHS [epv]),e,cords)::exceptions) 
+        |PARAM_OPTION(options) ->
+
+            let rec shortest_exception_list_r options = match options with 
+                |[] -> ((RHS options,UNEXPECTED_LEXEM(rh_value,rh_type),cords)::exceptions)
+                |top::rest ->
+                    (match (assign_type_check top []) with
+                    |[] -> exceptions 
+                    |_-> shortest_exception_list_r rest 
+                    )
+            in
+            shortest_exception_list_r options  
+
+        |CHOICE_VALUE(choices) as rh -> 
+
+
+            if List.mem rh_value choices then 
+                exceptions
+            else 
+                let e = UNEXPECTED_LEXEM(rh_value,rh_type) in
+                ((RHS [rh],e,cords)::exceptions)
+
+        |x -> 
+           let e = UNEXPECTED_LEXEM(rh_value,rh_type) in
+           let expected = (RHS [x]) in
+           ((expected,e,cords)::exceptions) 
+    in   
+    (match expected_rh_type with
+    |Some rh -> 
+        let exceptions = assign_type_check rh exceptions in
+        type_verify_r symbol_table rest exceptions scope
+    |None ->
+        let e = (UNKNOWN_IDENTIFIER(lh_value) ) in             
+        type_verify_r symbol_table rest ((scope,e,cords)::exceptions) scope
+    )
+    |ASSIGNMENT((lh_type,lh_value,cords),ASSIGNMENT_LIST(ls))::rest -> 
+        let expected_rh_type = 
+            match lh_type with
+            |KEYWORD when (Hashtbl.mem symbol_table (KEYWORD_SYMBOL(lh_value)) )  -> 
+               Hashtbl.find_opt symbol_table (KEYWORD_SYMBOL(lh_value)) 
+            |any_type when Hashtbl.mem symbol_table (TYPE_SYMBOL(any_type)) ->  
+               Hashtbl.find_opt symbol_table (TYPE_SYMBOL(any_type))
+            |_-> None
+        in
+
+     let  assignlist_type_check expected_rh_type ls exceptions= match expected_rh_type with
+         |(PROVINCE_MTTH as new_scope) 
+		 | (COUNTRY_MTTH as new_scope) 
+         | (PROVINCE_EFFECTS as new_scope) 
+		 | (PROVINCE_CONDITIONS as new_scope) 
+		 | (COUNTRY_EFFECTS as new_scope) 
+		 | (COUNTRY_CONDITIONS as new_scope) 
+		 | (POP_EFFECTS as new_scope) 
+		 | (POP_CONDITIONS as new_scope) 
+		 | (STATE_EFFECTS as new_scope) 
+         | (STATE_CONDITIONS as new_scope)
+		 | (PARAM_LIST(_) as new_scope)
+         | (APPEND_SYMBOLS(_) as new_scope)  ->
+            let symbol_tables = symbol_table_from_rhv new_scope in
+            let rec get_exceptions lst lowest_exception=
+              match lst with
+              | [] -> lowest_exception
+              | top_table::xs -> 
+                 (match (type_verify_r top_table ls exceptions (RHS([new_scope]))) with
+                 |[] ->  
+                         []
+                 |exceptions->
+                    get_exceptions xs  (exceptions@lowest_exception) 
+                 )
+            in
+            get_exceptions symbol_tables []
+
+         | (PARAM_OPTION(_) as new_scope) ->
+            let symbol_tables = symbol_table_from_rhv new_scope in
+            let rec get_exceptions lst lowest_exception=
+              match lst with
+              | [] -> 
+                    (match lowest_exception with
+                        |Some e -> e@exceptions
+                        |None -> 
+                                exceptions
+                    )
+              | top_table::xs -> 
+                 (match (type_verify_r top_table ls [] (RHS([new_scope]))) with
+                 |[] -> []
+                 |exceptions->
+                    get_exceptions xs (Some exceptions) 
+                 )
+            in
+            get_exceptions symbol_tables None
+            
+         | (x) -> 
+            let e = UNEXPECTED_ASSIGN_LIST(ls) in
+            ((RHS[x],e,cords)::exceptions)
+        in
+        (match expected_rh_type with
+        |Some rh -> 
+            let exceptions = assignlist_type_check rh ls exceptions in
+
+            type_verify_r symbol_table rest exceptions scope
+        |None -> 
+            let e = UNKNOWN_IDENTIFIER(lh_value) in
+
+            type_verify_r symbol_table rest ((scope,e,cords)::exceptions) scope
+        )
+    |(ASSIGNMENT ((_, _, cords), EXPR_EXCEPTION _) as assign) ::rest -> 
+        let e = (UNEXPECTED_ASSIGNMENT (assign)) in
+        type_verify_r symbol_table rest ((scope,e,cords)::exceptions) scope
+    |ASSIGN_EXCEPTION((expected,exception_val,cords))::rest -> 
+        type_verify_r symbol_table rest ((expected,exception_val,cords)::exceptions) scope
+
+    
+    | [] -> 
+           exceptions
+in
+List.rev (type_verify_r symbol_table assignments [] (RHS([PARAM_LIST(symbol_table)])))
+
+  
