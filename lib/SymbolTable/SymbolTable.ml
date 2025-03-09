c630fe1306d413944a1cd19f44268f1bc142296e
mjm2000 <holtzmanjimmy@yahoo.com>
2025-03-08 22:29:16 -0600

    Commit on run

--- lib/SymbolTable/SymbolTable.ml
+++ lib/SymbolTable/SymbolTable.ml
@@ -1,31 +1,3 @@
-module FunctionalHashTable : sig
-  type ('k, 'v) t
-  val empty : ('k, 'v) t
-  val add : ('k, 'v) t -> 'k -> 'v -> ('k, 'v) t
-  val find : ('k, 'v) t -> 'k -> 'v option
-  val remove : ('k, 'v) t -> 'k -> ('k, 'v) t
-  end = struct
-  module M = Map.Make(String) (* Change String to any comparable type *)
-
-  type ('k, 'v) t = 'v M.t
-
-  let empty = M.empty
-
-  let add table key value = M.add key value table
-
-  let find table key = 
-    try Some (M.find key table) 
-    with Not_found -> None
-
-  let remove table key = M.remove key table
-end
-
-
-
-
-
-
-
 let symbol_table_init symbols =
     let symbol_list_size = List.length symbols in
     let symbol_table = Hashtbl.create symbol_list_size in
