f11539586b8b2bb109d85c0b6a319091e55c499c
mjm2000 <holtzmanjimmy@yahoo.com>
2025-03-18 14:45:33 -0500

    Commit on run

--- lib/SyntaxChecker/SyntaxChecker.ml
+++ lib/SyntaxChecker/SyntaxChecker.ml
@@ -228,8 +228,9 @@ in
 List.map (function  
     |filepath, (symbol_table_key:string) ->
         let current_context= Hashtbl.find outer_symbol_table symbol_table_key in
+        let table = symbol_table_from_rhv current_context in
         let lexems = Lexer.lexer (Filename.concat home_dir filepath) in
         let assigns:(assignment list) = Parser.assignments lexems in
-        type_verify_r current_context assigns [] (RHS([DefinedTypeRight(symbol_table_key)])) 
+        type_verify_r table assigns [] (RHS([DefinedTypeRight(symbol_table_key)])) 
 ) directory 
   
