607bea564052f563e83eeabdf336d70797108e0a
mjm2000 <holtzmanjimmy@yahoo.com>
2025-03-18 15:00:52 -0500

    Commit on run

--- lib/SyntaxChecker/SyntaxChecker.ml
+++ lib/SyntaxChecker/SyntaxChecker.ml
@@ -61,9 +61,9 @@ let rec type_verify_r symbol_table (assignments:assignment list) exceptions scop
             |_,[] -> exceptions
             |VALUE_LIST(list_erh_type),(LEXEM(rh_type,rh_value,cords)::rest)  -> 
                 (match list_erh_type with
-                |PARAM_VALUE(erh_type) when Stdlib.== erh_type  rh_type  -> 
+                |PARAM_VALUE(erh_type:lexem_type) when erh_type = rh_type  -> 
                     assign_type_check expected_rh_type rest exceptions
-                |PARAM_VALUE(erh_type) when erh_type !=  rh_type  ->
+                |PARAM_VALUE(erh_type:lexem_type) when erh_type !=  rh_type  ->
                     let e = (TYPE_MISHMASH(lh_value, erh_type, rh_type)) in
                     let new_exceptions=((RHS [PARAM_VALUE(erh_type)],e,cords)::exceptions) in
                     assign_type_check expected_rh_type rest new_exceptions
