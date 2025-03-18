48a1c2447fa3554e3fcd93b0d415ae9f09b2222c
mjm2000 <holtzmanjimmy@yahoo.com>
2025-03-18 18:13:35 -0500

    Commit on run

--- lib/SymbolTable/SymbolTable.ml
+++ lib/SymbolTable/SymbolTable.ml
@@ -3,12 +3,12 @@ open Containers
 
 let symbol_table_init symbols =
     let symbol_list_size = List.length symbols in
-    let symbol_table = Hashtbl.create symbol_list_size in
+    let symbol_table = CCHashtbl.create symbol_list_size in
     let rec add_symbols symbols = match symbols with 
-        (*|(lh_value, rh_value)::tail when Hashtbl.mem symbol_table lh_value -> 
+        (*|(lh_value, rh_value)::tail when CCHashtbl.mem symbol_table lh_value -> 
             raise Invalid_Argument ("Symbol " ^ lh_value ^ " already exists") *)
         |(lh_value, rh_value)::tail ->
-            Hashtbl.add symbol_table lh_value rh_value;
+            CCHashtbl.add symbol_table lh_value rh_value;
             add_symbols tail
         | [] -> ()
     in
@@ -17,10 +17,10 @@ let symbol_table_init symbols =
 
 let append_table symbol_table symbols = 
     let rec add_symbols symbols = match symbols with 
-        |(lh_value, _)::_ when Hashtbl.mem symbol_table lh_value -> 
+        |(lh_value, _)::_ when CCHashtbl.mem symbol_table lh_value -> 
                 ()
         |(lh_value, rh_value)::tail ->
-            Hashtbl.add symbol_table lh_value rh_value;
+            CCHashtbl.add symbol_table lh_value rh_value;
             add_symbols tail
         | [] -> ()
     in
@@ -28,18 +28,18 @@ let append_table symbol_table symbols =
     
 
 let combine_table_pair table_take table_give = 
-    Hashtbl.iter (fun key value -> Hashtbl.replace table_take key value) table_give ;;
+    CCHashtbl.iter (fun key value -> Hashtbl.replace table_take key value) table_give ;;
 
 let combine_table_list tables = 
-  let table = Hashtbl.create 500 in
+  let table = CCHashtbl.create 500 in
   List.iter (fun t -> combine_table_pair table t) tables;
   table
 
 let lookup symbol_table symbol = 
-    match Hashtbl.find_opt symbol_table symbol with
+    match CCHashtbl.find_opt symbol_table symbol with
     | Some value -> value
     | None -> raise (Invalid_argument ("Symbol  not found"))
 
 
 let member symbol_table symbol = 
-    Hashtbl.mem symbol_table symbol;;
+    CCHashtbl.mem symbol_table symbol;;
