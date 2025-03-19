a11c703b097ae77fd55ed4041afb07405333ad33
mjm2000 <holtzmanjimmy@yahoo.com>
2025-03-18 19:44:40 -0500

    Commit on run

--- lib/SymbolTable/SymbolTable.ml
+++ lib/SymbolTable/SymbolTable.ml
@@ -37,7 +37,7 @@ let combine_table_list tables =
 let lookup symbol_table symbol = 
     match Hashtbl.find_opt symbol_table symbol with
     | Some value -> value
-    | None -> raise (Invalid_argument ("Symbol" ^symbol^ "not found"))
+    | None -> raise (Invalid_argument ("Symbol  not found"))
 
 
 let member symbol_table symbol = 
