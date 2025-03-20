bd7cd6b1efef409374506ae0fc1e15ce4c93eae5
mjm2000 <holtzmanjimmy@yahoo.com>
2025-03-20 14:08:27 -0500

    Commit on run

--- bin/main.ml
+++ bin/main.ml
@@ -19,8 +19,7 @@ let () =
     in
     let doc = "Paradox Mod Checker" in
     let info = Cmd.info "" ~doc in
-    let exceptions:(TypeDef.exception_value list list) = type_verify Victoria2.victoria2_symbol_table Victoria2.victoria2_paths "/mnt/c/Users/computer/Desktop/victoria_2_parser/HFM/"
-    in
+
     let term = Term.(const (fun game mh gh -> 
         match (mh,gh) with
         |(Some mod_home), (Some game_home) -> 
@@ -59,11 +58,6 @@ let () =
     Cmd.eval cmd |> Printf.printf "%i\n";
 
 
-    List.iter (fun x -> x
-    |> Parser.exceptions_string  
-    |> Printf.fprintf stdout "%s\n";)
-    exceptions;
-    () 
     
     
     
