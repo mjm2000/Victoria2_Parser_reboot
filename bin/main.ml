6a1c762870c549daa58b8c1f69feeb1a4fc7b504
mjm2000 <holtzmanjimmy@yahoo.com>
2025-03-21 23:11:55 -0500

    Commit on run

--- bin/main.ml
+++ bin/main.ml
@@ -5,7 +5,7 @@ open Re.Glob
 exception File_not_found of string
 let () = 
     let cursor = Runtime_events.create_cursor None in
-  let user_event = Runtime_events.User.register "my_program" Runtime_events.Type.unit in
+  let user_event = Runtime_events.User.register "allocation" Runtime_events.Type.unit in
   let callbacks = Callbacks.create ()
     |> Callbacks.add (Runtime_events.runtime_begin Runtime_events.Major) callback
     |> Callbacks.add (Runtime_events.runtime_begin Runtime_events.Minor) callback
