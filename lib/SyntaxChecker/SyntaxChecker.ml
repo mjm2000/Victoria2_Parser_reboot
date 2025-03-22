9ea766729ca33ee1c44419e893ed76577c7f9928
mjm2000 <holtzmanjimmy@yahoo.com>
2025-03-21 23:50:25 -0500

    Commit on run

--- lib/SyntaxChecker/SyntaxChecker.ml
+++ lib/SyntaxChecker/SyntaxChecker.ml
@@ -219,7 +219,9 @@ let rec type_verify_r symbol_table (assignments:assignment list) (exceptions:exc
             type_verify_r sub_table ls exceptions (RHS([new_scope])) 
         |PARAM_LIST(inner_table) as new_scope ->
             Printf.printf "PARAM_LIST\n ";
-            type_verify_r inner_table ls [] (RHS([new_scope]))  
+            let _ = type_verify_r inner_table ls [] (RHS([new_scope]))  
+            in
+            []
             
             
          | (x) -> 
