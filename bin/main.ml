b9760d255ab20009b2790ac7eb107395a1ba2e21
mjm2000 <holtzmanjimmy@yahoo.com>
2025-03-23 01:53:46 -0500

    Commit on run

--- bin/main.ml
+++ bin/main.ml
@@ -33,7 +33,9 @@ let () =
                         let abs_mod_file = Filename.concat mod_home file in
                         let abs_game_file = Filename.concat game_home file in
                         if (Sys.is_directory abs_mod_file) then
-                            let new_paths:string list = Array.to_list(Sys.readdir abs_mod_file) in
+                            let new_paths:string list = Array.fold_left (fun f rest ->
+                               (f,def)::rest 
+                            ) [] (Sys.readdir abs_mod_file) in
                             new_paths_r acc  (new_paths @rest_of_paths)
                         else if (Sys.is_directory abs_game_file) then
                             let new_paths:string list = Array.to_list(Sys.readdir abs_game_file) in
