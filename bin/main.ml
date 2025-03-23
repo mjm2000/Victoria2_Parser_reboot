d74f5a1c20e61cace4cdf839b08082e66ebb092d
mjm2000 <holtzmanjimmy@yahoo.com>
2025-03-23 01:58:17 -0500

    Commit on run

--- bin/main.ml
+++ bin/main.ml
@@ -39,7 +39,7 @@ let () =
                         else if (Sys.is_directory abs_game_file) then
                             let new_paths = Array.fold_left (fun  rest f ->
                                (f,def)::rest 
-                            ) [] (Sys.readdir abs_mod_file) in
+                            ) [] (Sys.readdir abs_game_file) in
                             new_paths_r acc  (new_paths @rest_of_paths)
 
                         else
