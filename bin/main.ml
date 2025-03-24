b867ddee9aa5a8141ebfdebc5b2e06336422ccba
mjm2000 <holtzmanjimmy@yahoo.com>
2025-03-23 22:56:16 -0500

    Commit on run

--- bin/main.ml
+++ bin/main.ml
@@ -30,9 +30,9 @@ let () =
 
                     |(file,def)::rest_of_paths ->
                         let abs_mod_file = Filename.concat mod_home file in
+                        let abs_game_file = Filename.concat game_home file in
 
                         Printf.eprintf  "%s:file\n" (abs_game_file);
-                        let abs_game_file = Filename.concat game_home file in
                         if (Sys.is_directory abs_mod_file) then
                             let new_paths = Array.fold_left (fun  rest f ->
                                ((Filename.concat file f),def)::rest 
