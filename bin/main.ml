3301649821763b5a3416eda6056ab5d207bb7458
mjm2000 <holtzmanjimmy@yahoo.com>
2025-03-23 22:55:24 -0500

    Commit on run

--- bin/main.ml
+++ bin/main.ml
@@ -45,14 +45,14 @@ let () =
                             new_paths_r acc  (new_paths @rest_of_paths)
 
                         else
-                            Printf.eprintf  "%s:file\n" (abs_game_file);
+                            (Printf.eprintf  "%s:file\n" (abs_game_file);
                             if (Sys.file_exists abs_mod_file) then
                                 new_paths_r ((abs_mod_file,def)::acc) rest_of_paths 
                             else if (Sys.file_exists abs_game_file) then
                                 new_paths_r ((abs_game_file,def)::acc) rest_of_paths 
                             else
                                 raise (File_not_found ("corrupted game files" ^abs_game_file) )
-                
+                            ) 
                     |[] -> acc
                     )
 
