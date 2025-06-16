571acff51aa469457f64b6c05797bdd53c439e2e
Jimmy Holtzman <holtzmanjimmy@yahoo.com>
2025-06-16 15:43:49 -0500

    update

--- bin/main.ml
+++ bin/main.ml
@@ -64,8 +64,9 @@ let () =
                  
                 in
                 let new_paths = new_paths_r [] paths in
-                let new_paths = ("mod_home_def",mod_home)::new_paths in
+               (* let new_paths = ("mod_home_def",mod_home)::new_paths in
                 let new_paths = ("game_home_def",game_home)::new_paths in
+            *)
 
                 let exceptions = type_verify symbol_table new_paths
                 in
