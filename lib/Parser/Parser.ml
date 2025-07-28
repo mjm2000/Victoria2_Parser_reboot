b5598956f481770bb6cb827279483d06d12c55e9
Jimmy Holtzman <holtzmanjimmy@yahoo.com>
2025-07-28 15:55:54 -0500

    update

--- lib/Parser/Parser.ml
+++ lib/Parser/Parser.ml
@@ -95,11 +95,9 @@ and expression lexems file =
         (match rest with
         |_::(EQ,_,_)::_ ->
                 let v, rest = assignment_list rest file in 
-                Printf.printf "assignment lexem list: %s\n" (Output.string_expr (ASSIGNMENT_LIST(v)) );
                 ASSIGNMENT_LIST v, rest
         |_ -> 
             let v, rest = lexem_list rest file in
-            Printf.printf "Expression lexem list: %s\n" (Output.string_expr (LEXEM_LIST(v)));
             LEXEM_LIST(v), rest
         )
     | (RB, _, cords) :: rest -> 
