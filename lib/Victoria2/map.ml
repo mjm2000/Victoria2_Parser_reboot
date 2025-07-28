7ab8ee2412c76dbe17ff0c828d57732cd6b0c67b
Jimmy Holtzman <holtzmanjimmy@yahoo.com>
2025-07-28 00:27:50 -0500

    update

--- lib/Victoria2/map.ml
+++ lib/Victoria2/map.ml
@@ -12,5 +12,5 @@ let continents = symbol_table_init [
 
 ]
 let regions = symbol_table_init [
-    (Catalog("region",Identifier), WholeNumber ); 
+    (Catalog("region",Value Keyword), WholeNumber ); 
 ]
