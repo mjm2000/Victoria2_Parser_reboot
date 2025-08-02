4c1814cdc4062333a7372a76888cd24215d1e61a
Jimmy Holtzman <holtzmanjimmy@yahoo.com>
2025-08-01 21:24:12 -0500

    update

--- lib/Victoria2/effects.ml
+++ lib/Victoria2/effects.ml
@@ -2,6 +2,7 @@ open SymbolTable
 open TypeDef
 
 let country_effects =  symbol_table_init [
+    (Literal "tech_school",Type "tech_school");
 (Type "region",Type "state_effects_def");
 (Literal("poor_strata"),Type "pop_effects_def");
 (Literal("middle_strata"),Type "pop_effects_def");
